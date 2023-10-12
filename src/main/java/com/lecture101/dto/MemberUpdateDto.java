dea8542a0923bbecefb89b6f4d70f6e176883eaa
Junseok Lee <112673222+freedomseok8047@users.noreply.github.com>
2023-10-12 18:55:39 +0900

    231012 8교시 종료 : MemberUpdateDto 생성

---
+++ src/main/java/com/lecture101/dto/MemberUpdateDto.java
@@ -0,0 +1,45 @@
+package com.lecture101.dto;
+
+import com.lecture101.entity.Member;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.modelmapper.ModelMapper;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class MemberUpdateDto {
+    private Long id;
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String name;
+
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
+    private String email;
+
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
+    private String address;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min=8, max=16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
+    private String CurrentPassword;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min=8, max=16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
+    private String newPassword;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min=8, max=16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
+    private String ConfimPassword;
+
+
+    private static ModelMapper modelMapper = new ModelMapper();
+    public static MemberUpdateDto of(Member member) {
+        return modelMapper.map(member,MemberUpdateDto.class);
+    }
+}
